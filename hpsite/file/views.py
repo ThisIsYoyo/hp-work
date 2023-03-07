69ed90b348e1882ffe2895abe6bcf2d81b57ece6
YoyoHsieh21 <yoyo.hsieh@gofreight.co>
2023-03-07 16:07:38 +0800

    feat(file): add index using rest-framework

--- hpsite/file/views.py
+++ hpsite/file/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+# @renderer_classes
+def index(request):
+    return Response('Hello, World. This is simple Response for index!', status=status.HTTP_200_OK)
